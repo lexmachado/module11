3c2f26b92cf473275e94157810a062c0ca08fb62
Lex <lexmachado998@gmail.com>
2023-09-26 04:12:07 -0400

    update

--- Develop/public/javascript.js
+++ Develop/public/javascript.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-
+const path = require('./api.notes');
+const api = require('./routes/index.html');
+const api = require('./routes/notes.html');
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'notes.html'));
 });
-
+app.use(clog);
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'notes.html'))
+);
 app.listen(3000, () => {
   console.log('Server is running');
 });
 const typeDefs = require('./notes.html');
 const resolvers = require('./index.html');
+const { clog } = require();
 
 module.exports = { typeDefs, resolvers };
  
